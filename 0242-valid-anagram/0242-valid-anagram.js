dd143aa86cd3924596d3a5c4c85979de93363fe6
sshhamiill <sshhamiill@gmail.com>
2026-09-01 10:21:44 +0530

    Time: 30 ms (15.22%), Space: 57.4 MB (53.5%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+   let first=s.split("").sort().join("")
+   let second=t.split("").sort().join("") 
+   return first === second
+}
